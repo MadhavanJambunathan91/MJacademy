16b3884c9ef74c371586098c28ae828f3927efc3
MadhavanJambunathan91 <madhavan915@gmail.com>
2025-04-22 09:27:35 +0530

    This is asian guy changes

---
+++ src/test/java/MJacademy/test/Gittraining.java
@@ -0,0 +1,13 @@
+package MJacademy.test;
+
+public class Gittraining {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+           String training = "Hello world";
+           System.out.println(training);
+           
+	}
+
+}
