f8fe200a663d3df03b97a596aafedb90722c69c8
MadhavanJambunathan91 <madhavan915@gmail.com>
2025-04-29 09:06:33 +0530

    American develop team

--- src/test/java/MJacademy/test/Gittraining.java
+++ src/test/java/MJacademy/test/Gittraining.java
@@ -4,19 +4,20 @@ public class Gittraining {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		
-           String training = "Hello world";
-           System.out.println(training);
-           
-           System.out.println("This is git og");
-           
-  
-           
+
+		String training = "Hello world";
+		System.out.println(training);
+
+		System.out.println("This is git og");
+
+	}
+
+	public void newbranch() {
+		System.out.println("This is new branch");
 	}
 	
-	public void newbranch ()
-    {
- 	   System.out.println("This is new branch");
-    }
+	public void secondbranchdevelop() {
+		System.out.println("This is new branch develop");
+	}
 
 }
